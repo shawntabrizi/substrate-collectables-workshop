37fb203f50a1421b4cba2edb0083497129b06ff0
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-01-18 16:39:53 +0100

    creating more templates

---
+++ 2/assets/1-template.rs
@@ -0,0 +1,62 @@
+use srml_support::{StorageValue, StorageMap, dispatch::Result};
+use system::ensure_signed;
+use runtime_primitives::traits::{As, Hash};
+use rstd::prelude::*;
+
+#[derive(Encode, Decode, Default, Clone, PartialEq, Debug)]
+pub struct Kitty<Hash, Balance> {
+    id: Hash,
+    name: Vec<u8>,
+    dna: Hash,
+    price: Balance,
+    gen: u64,
+}
+
+pub trait Trait: balances::Trait {}
+
+decl_storage! {
+    trait Store for Module<T: Trait> as KittyStorage {
+        // ACTION: Add three new storage items: 
+        //      - `Kitties` which maps a `T::Hash` to a `Kitty<T::Hash, T::Balance>`
+        //      - `KittyOwner` which maps a `T::Hash` to an `Option<T::AccountId>`
+
+        // ACTION: Update `OwnedKitty` to store a `T::Hash`
+        OwnedKitty get(kitty_of_owner): map T::AccountId => Kitty<T::Hash, T::Balance>;
+
+        Nonce: u64;
+    }
+}
+
+decl_module! {
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+
+        fn create_kitty(origin, name: Vec<u8>) -> Result {
+            let sender = ensure_signed(origin)?;
+
+            // ACTION: Generate a `random_hash` using: 
+            //      - `<system::Module<T>>::random_seed()`
+            //      - `sender`
+            //      - `Nonce`
+
+            // ACTION: "Ensure" our `random_hash` does not collide with an existing token
+
+            // ACTION: Update our Kitty to use this `random_hash` as the `id` and the `dna`
+            let new_kitty = Kitty {
+                                id: <T as system::Trait>::Hashing::hash_of(&0),
+                                name: name,
+                                dna: <T as system::Trait>::Hashing::hash_of(&0),
+                                price: <T::Balance as As<u64>>::sa(0),
+                                gen: 0,
+                            };
+
+            // ACTION: "Insert" the storage for `Kitties`, should point from our kitty's id to the `Kitty` object
+            // ACTION: "Insert" the storage for `KittyOwner`, should point from our kitty's id to the owner
+            // ACTION: Update the OwnedKitty storage below to store the kitty's id rather than the `Kitty` object
+            <OwnedKitty<T>>::insert(&sender, new_kitty);
+
+            <Nonce<T>>::mutate(|n| *n += 1);
+
+            Ok(())
+        }
+    }
+}
---
+++ 2/assets/2-template.rs
@@ -0,0 +1,73 @@
+use parity_codec::Encode;
+use srml_support::{StorageValue, StorageMap, dispatch::Result};
+use system::ensure_signed;
+use runtime_primitives::traits::{As, Hash};
+use rstd::prelude::*;
+
+#[derive(Encode, Decode, Default, Clone, PartialEq, Debug)]
+pub struct Kitty<Hash, Balance> {
+    id: Hash,
+    name: Vec<u8>,
+    dna: Hash,
+    price: Balance,
+    gen: u64,
+}
+
+pub trait Trait: balances::Trait {
+    // ACTION: Define your `Event` type here
+    //      REMINDER: It needs these traits: `From<Event<Self>> + Into<<Self as system::Trait>::Event>`
+}
+
+decl_event!(
+    pub enum Event<T>
+    where
+        <T as system::Trait>::AccountId,
+        <T as system::Trait>::Hash
+    {
+        // ACTION: Add a `Created` event which includes an `AccountId` and a `Hash`
+    }
+);
+
+decl_storage! {
+    trait Store for Module<T: Trait> as KittyStorage {
+        Kitties get(kitty): map T::Hash => Kitty<T::Hash, T::Balance>;
+        KittyOwner get(owner_of): map T::Hash => Option<T::AccountId>;
+        OwnedKitty get(kitty_of_owner): map T::AccountId => T::Hash;
+
+        Nonce: u64;
+    }
+}
+
+decl_module! {
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+
+        // ACTION: Define your generic `deposit_event<T>()` function
+        //      REMINDER: You can use the default implementation provided by the `decl_module!` macro with `default`
+
+        fn create_kitty(origin, name: Vec<u8>) -> Result {
+            let sender = ensure_signed(origin)?;
+
+            let nonce = <Nonce<T>>::get();
+            let random_hash = (<system::Module<T>>::random_seed(), &sender, nonce)
+                                .using_encoded(<T as system::Trait>::Hashing::hash);
+
+            ensure!(!<KittyOwner<T>>::exists(random_hash), "Kitty already exists");
+
+            let new_kitty = Kitty {
+                                id: random_hash,
+                                name: name,
+                                dna: random_hash,
+                                price: <T::Balance as As<u64>>::sa(0),
+                                gen: 0,
+                            };
+
+            <Kitties<T>>::insert(random_hash, new_kitty);
+            <KittyOwner<T>>::insert(random_hash, &sender);
+            <OwnedKitty<T>>::insert(&sender, random_hash);
+
+            <Nonce<T>>::mutate(|n| *n += 1);
+
+            Ok(())
+        }
+    }
+}
