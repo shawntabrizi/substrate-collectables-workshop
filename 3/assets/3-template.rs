7745b7efd0ceb3b02a85b47f137e71f1e48116a7
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-01-18 11:57:49 +0100

    templating code backwards

---
+++ 3/assets/3-template.rs
@@ -0,0 +1,221 @@
+use parity_codec::Encode;
+use srml_support::{StorageValue, StorageMap, dispatch::Result};
+use system::ensure_signed;
+use runtime_primitives::traits::{As, Hash};
+use rstd::prelude::*;
+
+#[derive(Encode, Decode, Default, Clone, PartialEq, Debug)]
+pub struct Kitty<Hash, Balance> {
+    id: Hash,
+    name: Vec<u8>,
+    dna: Hash,
+    price: Balance,
+    gen: u64,
+}
+
+pub trait Trait: balances::Trait {
+    type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+}
+
+decl_event!(
+    pub enum Event<T>
+    where
+        <T as system::Trait>::AccountId,
+        <T as system::Trait>::Hash,
+        <T as balances::Trait>::Balance
+    {
+        Created(AccountId, Hash),
+        Transferred(AccountId, AccountId, Hash),
+        PriceSet(AccountId, Hash, Balance),
+        // ACTION: Create a `Bought` event here
+    }
+);
+
+decl_storage! {
+    trait Store for Module<T: Trait> as KittyStorage {
+        Kitties get(kitty): map T::Hash => Kitty<T::Hash, T::Balance>;
+        KittyOwner get(owner_of): map T::Hash => Option<T::AccountId>;
+
+        AllKittiesArray get(kitty_by_index): map u64 => T::Hash;
+        AllKittiesCount get(all_kitties_count): u64;
+        AllKittiesIndex: map T::Hash => u64;
+
+        OwnedKittiesArray get(kitty_of_owner_by_index): map (T::AccountId, u64) => T::Hash;
+        OwnedKittiesCount get(owned_kitty_count): map T::AccountId => u64;
+        OwnedKittiesIndex: map T::Hash => u64;
+        
+        Nonce: u64;
+    }
+}
+
+decl_module! {
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+
+        fn deposit_event<T>() = default;
+
+        fn transfer(origin, to: T::AccountId, kitty_id: T::Hash) -> Result {
+            let sender = ensure_signed(origin)?;
+
+            let owner = match Self::owner_of(kitty_id) {
+                Some(o) => o,
+                None => return Err("No owner for this kitty"),
+            };
+            ensure!(owner == sender, "You do not own this kitty");
+
+            Self::_transfer_from(sender, to, kitty_id)?;
+
+            Ok(())
+        }
+
+        fn create_kitty(origin, name: Vec<u8>) -> Result {
+            let sender = ensure_signed(origin)?;
+            let nonce = <Nonce<T>>::get();
+            let random_hash = (<system::Module<T>>::random_seed(), &sender, nonce)
+                                .using_encoded(<T as system::Trait>::Hashing::hash);
+
+            let new_kitty = Kitty {
+                                id: random_hash,
+                                name: name,
+                                dna: random_hash,
+                                price: <T::Balance as As<u64>>::sa(0),
+                                gen: 0,
+                            };
+
+            Self::_mint(sender, random_hash, new_kitty)?;
+            
+            <Nonce<T>>::mutate(|n| *n += 1);
+
+            Ok(())
+        }
+
+        fn set_price(origin, kitty_id: T::Hash, new_price: T::Balance) -> Result {
+            let sender = ensure_signed(origin)?;
+
+            ensure!(<Kitties<T>>::exists(kitty_id), "This cat does not exist");
+
+            let owner = match Self::owner_of(kitty_id) {
+                Some(c) => c,
+                None => return Err("No owner for this kitty"),
+            };
+            ensure!(owner == sender, "You do not own this cat");
+
+            let mut kitty = Self::kitty(kitty_id);
+            kitty.price = new_price;
+
+            <Kitties<T>>::insert(kitty_id, kitty);
+
+            Self::deposit_event(RawEvent::PriceSet(sender, kitty_id, new_price));
+
+            Ok(())
+        }
+        
+        fn buy_cat(origin, kitty_id: T::Hash, max_price: T::Balance) -> Result {
+            let sender = ensure_signed(origin)?;
+
+            // ACTION: Check the kitty `exists()`
+
+            // ACTION: Get the `owner` of the kitty if it exists, otherwise return an `Err()`
+            // ACTION: Check that the `sender` is not the `owner`
+
+            let mut kitty = Self::kitty(kitty_id);
+
+            // ACTION: Get the `kitty_price` and check that it is not zero
+            //      HINT:  `runtime_primitives::traits::Zero` allows you to call `kitty_price.is_zero()` which returns a bool
+
+            // ACTION: Check `kitty_price` is less than or equal to max_price
+
+            // ACTION: "Try" to `decrease_free_balance()` of the sender
+            // ACTION: `increase_free_balance()` of the owner
+
+            // ACTION: Transfer the kitty
+
+            // ACTION: Reset kitty price back to zero, and update the storage
+
+            // ACTION: Create an event for the cat being bought with relevant details
+            //      - new owner
+            //      - old owner
+            //      - the kitty id
+            //      - the price sold for
+
+            Ok(())
+        }
+    }
+}
+
+impl<T: Trait> Module<T> {
+    fn _mint(to: T::AccountId, kitty_id: T::Hash, new_kitty: Kitty<T::Hash, T::Balance>) -> Result {
+        ensure!(!<KittyOwner<T>>::exists(kitty_id), "Kitty already exists");
+
+        let owned_kitty_count = Self::owned_kitty_count(&to);
+
+        let new_owned_kitty_count = match owned_kitty_count.checked_add(1) {
+            Some(c) => c,
+            None => return Err("Overflow adding a new kitty to account balance"),
+        };
+
+        let all_kitties_count = Self::all_kitties_count();
+
+        let new_all_kitties_count = match all_kitties_count.checked_add(1) {
+            Some (c) => c,
+            None => return Err("Overflow adding a new kitty to total supply"),
+        };
+
+        <Kitties<T>>::insert(kitty_id, new_kitty);
+        <KittyOwner<T>>::insert(kitty_id, &to);
+
+        <AllKittiesArray<T>>::insert(all_kitties_count, kitty_id);
+        <AllKittiesCount<T>>::put(new_all_kitties_count);
+        <AllKittiesIndex<T>>::insert(kitty_id, all_kitties_count);
+
+        <OwnedKittiesArray<T>>::insert((to.clone(), owned_kitty_count), kitty_id);
+        <OwnedKittiesCount<T>>::insert(&to, new_owned_kitty_count);
+        <OwnedKittiesIndex<T>>::insert(kitty_id, owned_kitty_count);
+
+        Self::deposit_event(RawEvent::Created(to, kitty_id));
+
+        Ok(())
+    }
+
+    fn _transfer_from(from: T::AccountId, to: T::AccountId, kitty_id: T::Hash) -> Result {
+        let owner = match Self::owner_of(kitty_id) {
+            Some(c) => c,
+            None => return Err("No owner for this kitty"),
+        };
+
+        ensure!(owner == from, "'from' account does not own this kitty");
+
+        let owned_kitty_count_from = Self::owned_kitty_count(&from);
+        let owned_kitty_count_to = Self::owned_kitty_count(&to);
+
+        let new_owned_kitty_count_to = match owned_kitty_count_to.checked_add(1) {
+            Some(c) => c,
+            None => return Err("Transfer causes overflow of 'to' kitty balance"),
+        };
+
+        let new_owned_kitty_count_from = match owned_kitty_count_from.checked_sub(1) {
+            Some (c) => c,
+            None => return Err("Transfer causes underflow of 'from' kitty balance"),
+        };
+
+        // "Swap and pop"
+        let kitty_index = <OwnedKittiesIndex<T>>::get(kitty_id);
+        if kitty_index != new_owned_kitty_count_from {
+            let last_kitty_id = <OwnedKittiesArray<T>>::get((from.clone(), new_owned_kitty_count_from));
+            <OwnedKittiesArray<T>>::insert((from.clone(), kitty_index), last_kitty_id);
+            <OwnedKittiesIndex<T>>::insert(last_kitty_id, kitty_index);
+        }
+
+        <KittyOwner<T>>::insert(&kitty_id, &to);
+        <OwnedKittiesIndex<T>>::insert(kitty_id, owned_kitty_count_to);
+
+        <OwnedKittiesArray<T>>::remove((from.clone(), new_owned_kitty_count_from));
+        <OwnedKittiesArray<T>>::insert((to.clone(), owned_kitty_count_to), kitty_id);
+
+        <OwnedKittiesCount<T>>::insert(&from, new_owned_kitty_count_from);
+        <OwnedKittiesCount<T>>::insert(&to, new_owned_kitty_count_to);
+        
+        Self::deposit_event(RawEvent::Transferred(from, to, kitty_id));
+        
+        Ok(())
+    }
+}
---
+++ 3/assets/4-template.rs
@@ -0,0 +1,256 @@
+use parity_codec::Encode;
+use srml_support::{StorageValue, StorageMap, dispatch::Result};
+use system::ensure_signed;
+use runtime_primitives::traits::{As, Hash, Zero};
+use rstd::prelude::*;
+use rstd::cmp;
+
+#[derive(Encode, Decode, Default, Clone, PartialEq, Debug)]
+pub struct Kitty<Hash, Balance> {
+    id: Hash,
+    name: Vec<u8>,
+    dna: Hash,
+    price: Balance,
+    gen: u64,
+}
+
+pub trait Trait: balances::Trait {
+    type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+}
+
+decl_event!(
+    pub enum Event<T>
+    where
+        <T as system::Trait>::AccountId,
+        <T as system::Trait>::Hash,
+        <T as balances::Trait>::Balance
+    {
+        Created(AccountId, Hash),
+        Transferred(AccountId, AccountId, Hash),
+        PriceSet(AccountId, Hash, Balance),
+        Bought(AccountId, AccountId, Hash, Balance),
+    }
+);
+
+decl_storage! {
+    trait Store for Module<T: Trait> as KittyStorage {
+        Kitties get(kitty): map T::Hash => Kitty<T::Hash, T::Balance>;
+        KittyOwner get(owner_of): map T::Hash => Option<T::AccountId>;
+
+        AllKittiesArray get(kitty_by_index): map u64 => T::Hash;
+        AllKittiesCount get(all_kitties_count): u64;
+        AllKittiesIndex: map T::Hash => u64;
+
+        OwnedKittiesArray get(kitty_of_owner_by_index): map (T::AccountId, u64) => T::Hash;
+        OwnedKittiesCount get(owned_kitty_count): map T::AccountId => u64;
+        OwnedKittiesIndex: map T::Hash => u64;
+        
+        Nonce: u64;
+    }
+}
+
+decl_module! {
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+
+        fn deposit_event<T>() = default;
+
+        fn transfer(origin, to: T::AccountId, kitty_id: T::Hash) -> Result {
+            let sender = ensure_signed(origin)?;
+
+            let owner = match Self::owner_of(kitty_id) {
+                Some(o) => o,
+                None => return Err("No owner for this kitty"),
+            };
+            ensure!(owner == sender, "You do not own this kitty");
+
+            Self::_transfer_from(sender, to, kitty_id)?;
+
+            Ok(())
+        }
+
+        fn create_kitty(origin, name: Vec<u8>) -> Result {
+            let sender = ensure_signed(origin)?;
+            let nonce = <Nonce<T>>::get();
+            let random_hash = (<system::Module<T>>::random_seed(), &sender, nonce)
+                                .using_encoded(<T as system::Trait>::Hashing::hash);
+
+            let new_kitty = Kitty {
+                                id: random_hash,
+                                name: name,
+                                dna: random_hash,
+                                price: <T::Balance as As<u64>>::sa(0),
+                                gen: 0,
+                            };
+
+            Self::_mint(sender, random_hash, new_kitty)?;
+            
+            <Nonce<T>>::mutate(|n| *n += 1);
+
+            Ok(())
+        }
+
+        fn set_price(origin, kitty_id: T::Hash, new_price: T::Balance) -> Result {
+            let sender = ensure_signed(origin)?;
+
+            ensure!(<Kitties<T>>::exists(kitty_id), "This cat does not exist");
+
+            let owner = match Self::owner_of(kitty_id) {
+                Some(c) => c,
+                None => return Err("No owner for this kitty"),
+            };
+            ensure!(owner == sender, "You do not own this cat");
+
+            let mut kitty = Self::kitty(kitty_id);
+            kitty.price = new_price;
+
+            <Kitties<T>>::insert(kitty_id, kitty);
+
+            Self::deposit_event(RawEvent::PriceSet(sender, kitty_id, new_price));
+
+            Ok(())
+        }
+        
+        fn buy_cat(origin, kitty_id: T::Hash, max_price: T::Balance) -> Result {
+            let sender = ensure_signed(origin)?;
+
+            ensure!(<Kitties<T>>::exists(kitty_id), "This cat does not exist");
+
+            let owner = match Self::owner_of(kitty_id) {
+                Some(o) => o,
+                None => return Err("No owner for this kitty"),
+            };
+            ensure!(owner != sender, "You can't buy your own cat");
+
+            let mut kitty = Self::kitty(kitty_id);
+
+            let kitty_price = kitty.price;
+            ensure!(!kitty_price.is_zero(), "The cat you want to buy is not for sale");
+            ensure!(kitty_price <= max_price, "The cat you want to buy costs more than your max price");
+
+            // TODO: This payment logic needs to be updated
+            <balances::Module<T>>::decrease_free_balance(&sender, kitty_price)?;
+            <balances::Module<T>>::increase_free_balance_creating(&owner, kitty_price);
+
+            Self::_transfer_from(owner.clone(), sender.clone(), kitty_id)?;
+
+            kitty.price = <T::Balance as As<u64>>::sa(0);
+            <Kitties<T>>::insert(kitty_id, kitty);
+
+            Self::deposit_event(RawEvent::Bought(sender, owner, kitty_id, kitty_price));
+
+            Ok(())
+        }
+
+        fn breed_cat(origin, name: Vec<u8>, kitty_id_1: T::Hash, kitty_id_2: T::Hash) -> Result{
+            let sender = ensure_signed(origin)?;
+
+            // ACTION: Check both kitty 1 and kitty 2 "exists"
+
+            // ACTION: Generate a `random_hash` using the <Nonce<T>>
+
+            let kitty_1 = Self::kitty(kitty_id_1);
+            let kitty_2 = Self::kitty(kitty_id_2);
+
+            // Our gene splicing algorithm, feel free to make it your own
+            let mut final_dna = kitty_1.dna;
+
+            for (i, (dna_2_element, r)) in kitty_2.dna.as_ref().iter().zip(random_hash.as_ref().iter()).enumerate() {
+                if r % 2 == 0 {
+                    final_dna.as_mut()[i] = *dna_2_element;
+                }
+            }
+
+            // ACTION: Create a `new_kitty` using: 
+            //      - `random_hash` as `id`
+            //      - `name` as `name`
+            //      - `final_dna` as `dna`
+            //      - 0 as `price`
+            //      - the max of the parent's `gen` + 1
+            //          - Hint: `rstd::cmp::max(1, 5) + 1` is `6`
+
+            // ACTION: `_mint()` your new kitty
+
+            // ACTION: Update the <Nonce<T>>
+
+            Ok(())
+        }
+    }
+}
+
+impl<T: Trait> Module<T> {
+    fn _mint(to: T::AccountId, kitty_id: T::Hash, new_kitty: Kitty<T::Hash, T::Balance>) -> Result {
+        ensure!(!<KittyOwner<T>>::exists(kitty_id), "Kitty already exists");
+
+        let owned_kitty_count = Self::owned_kitty_count(&to);
+
+        let new_owned_kitty_count = match owned_kitty_count.checked_add(1) {
+            Some(c) => c,
+            None => return Err("Overflow adding a new kitty to account balance"),
+        };
+
+        let all_kitties_count = Self::all_kitties_count();
+
+        let new_all_kitties_count = match all_kitties_count.checked_add(1) {
+            Some (c) => c,
+            None => return Err("Overflow adding a new kitty to total supply"),
+        };
+
+        <Kitties<T>>::insert(kitty_id, new_kitty);
+        <KittyOwner<T>>::insert(kitty_id, &to);
+
+        <AllKittiesArray<T>>::insert(all_kitties_count, kitty_id);
+        <AllKittiesCount<T>>::put(new_all_kitties_count);
+        <AllKittiesIndex<T>>::insert(kitty_id, all_kitties_count);
+
+        <OwnedKittiesArray<T>>::insert((to.clone(), owned_kitty_count), kitty_id);
+        <OwnedKittiesCount<T>>::insert(&to, new_owned_kitty_count);
+        <OwnedKittiesIndex<T>>::insert(kitty_id, owned_kitty_count);
+
+        Self::deposit_event(RawEvent::Created(to, kitty_id));
+
+        Ok(())
+    }
+
+    fn _transfer_from(from: T::AccountId, to: T::AccountId, kitty_id: T::Hash) -> Result {
+        let owner = match Self::owner_of(kitty_id) {
+            Some(c) => c,
+            None => return Err("No owner for this kitty"),
+        };
+
+        ensure!(owner == from, "'from' account does not own this kitty");
+
+        let owned_kitty_count_from = Self::owned_kitty_count(&from);
+        let owned_kitty_count_to = Self::owned_kitty_count(&to);
+
+        let new_owned_kitty_count_to = match owned_kitty_count_to.checked_add(1) {
+            Some(c) => c,
+            None => return Err("Transfer causes overflow of 'to' kitty balance"),
+        };
+
+        let new_owned_kitty_count_from = match owned_kitty_count_from.checked_sub(1) {
+            Some (c) => c,
+            None => return Err("Transfer causes underflow of 'from' kitty balance"),
+        };
+
+        // "Swap and pop"
+        let kitty_index = <OwnedKittiesIndex<T>>::get(kitty_id);
+        if kitty_index != new_owned_kitty_count_from {
+            let last_kitty_id = <OwnedKittiesArray<T>>::get((from.clone(), new_owned_kitty_count_from));
+            <OwnedKittiesArray<T>>::insert((from.clone(), kitty_index), last_kitty_id);
+            <OwnedKittiesIndex<T>>::insert(last_kitty_id, kitty_index);
+        }
+
+        <KittyOwner<T>>::insert(&kitty_id, &to);
+        <OwnedKittiesIndex<T>>::insert(kitty_id, owned_kitty_count_to);
+
+        <OwnedKittiesArray<T>>::remove((from.clone(), new_owned_kitty_count_from));
+        <OwnedKittiesArray<T>>::insert((to.clone(), owned_kitty_count_to), kitty_id);
+
+        <OwnedKittiesCount<T>>::insert(&from, new_owned_kitty_count_from);
+        <OwnedKittiesCount<T>>::insert(&to, new_owned_kitty_count_to);
+        
+        Self::deposit_event(RawEvent::Transferred(from, to, kitty_id));
+        
+        Ok(())
+    }
+}
