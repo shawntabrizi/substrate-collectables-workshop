a589f0ef3b3b6cccd2b4abdebe50c91d082522e8
Shawn Tabrizi <shawntabrizi@gmail.com>
2024-08-03 02:52:59 -0400

    add starting template

---
+++ steps/2/README.md
@@ -0,0 +1,15 @@
+# Substrate Collectables Workshop: Starting Template
+
+This is the starting template for: https://github.com/shawntabrizi/substrate-collectables-workshop
+
+## Cheat Sheet
+
+You should be able to run all 3 of these commands at the end of every step without any errors or warnings.
+
+```bash
+cargo +nightly fmt
+cargo +nightly clippy
+cargo test
+```
+
+I recommend you do this to ensure that you code is always compiles, is well formatted, and all that.
---
+++ steps/2/src/lib.rs
@@ -0,0 +1,38 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+mod impls;
+
+pub use pallet::*;
+
+#[frame_support::pallet(dev_mode)]
+pub mod pallet {
+	use super::*;
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+
+	#[pallet::pallet]
+	pub struct Pallet<T>(core::marker::PhantomData<T>);
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+	}
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		Created { owner: T::AccountId },
+	}
+
+	#[pallet::error]
+	pub enum Error<T> {}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		pub fn create_kitty(origin: OriginFor<T>) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+			Self::mint(who)?;
+			Ok(())
+		}
+	}
+}
