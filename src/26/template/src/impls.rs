374eeb7ee84a0a4ace6bd866c7cfd955ec2f011d
Shawn Tabrizi <shawntabrizi@gmail.com>
2024-08-03 23:53:51 -0400

    mdBook generated from gitorial

--- src/26/template/src/impls.rs
+++ src/26/template/src/impls.rs
@@ -21,7 +21,7 @@ impl<T: Config> Pallet<T> {
 
 	pub fn mint(owner: T::AccountId, dna: [u8; 32]) -> DispatchResult {
 		/* 🚧 TODO 🚧: Add the `price` field set to `None` when initializing the `Kitty` struct. */
-		let kitty = Kitty { dna, owner: owner.clone(), price: None };
+		let kitty = Kitty { dna, owner: owner.clone() };
 		// Check if the kitty does not already exist in our storage map
 		ensure!(!Kitties::<T>::contains_key(dna), Error::<T>::DuplicateKitty);
 
