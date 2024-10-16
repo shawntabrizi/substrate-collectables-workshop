9f0e568653525bf28f97317de89e199ad58b6bdd
Shawn Tabrizi <shawntabrizi@gmail.com>
2024-10-16 09:00:15 -0400

    section: storage basics

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,6 @@ pub mod pallet {
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 	}
 
-	/* 🚧 TODO 🚧: Learn about Pallet Events. */
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
