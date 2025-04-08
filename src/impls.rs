ffdd634f0e16b0340cfaff4bbdeed291e61539c2
Shawn Tabrizi <shawntabrizi@gmail.com>
2025-04-08 06:52:26 -0400

    template: counter logic

--- src/impls.rs
+++ src/impls.rs
@@ -3,6 +3,12 @@ use frame::prelude::*;
 
 impl<T: Config> Pallet<T> {
 	pub fn mint(owner: T::AccountId) -> DispatchResult {
+		/* 🚧 TODO 🚧:
+			- `get` the `current_count` of kitties.
+			- `unwrap_or` set the count to `0`.
+			- Create `new_count` by adding one to the `current_count`.
+			- `set` the `new_count` of kitties.
+		*/
 		Self::deposit_event(Event::<T>::Created { owner });
 		Ok(())
 	}
