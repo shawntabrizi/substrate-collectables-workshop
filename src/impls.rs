4a2bec156b4151d90cf8adfd6932b2795564004d
Shawn Tabrizi <shawntabrizi@gmail.com>
2025-04-08 06:52:27 -0400

    template: use kitties map

--- src/impls.rs
+++ src/impls.rs
@@ -2,9 +2,11 @@ use super::*;
 use frame::prelude::*;
 
 impl<T: Config> Pallet<T> {
+	/* 🚧 TODO 🚧: Update this function signature to include `dna` which is type `[u8; 32]`. */
 	pub fn mint(owner: T::AccountId) -> DispatchResult {
 		let current_count: u32 = CountForKitties::<T>::get();
 		let new_count = current_count.checked_add(1).ok_or(Error::<T>::TooManyKitties)?;
+		/* 🚧 TODO 🚧: In the `Kitties` map, under the key `dna`, insert `()`. */
 		CountForKitties::<T>::set(new_count);
 		Self::deposit_event(Event::<T>::Created { owner });
 		Ok(())
