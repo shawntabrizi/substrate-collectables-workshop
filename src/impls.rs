e0359d21e659c7dea0556e3e4ca6916c10599fba
Shawn Tabrizi <shawntabrizi@gmail.com>
2025-04-08 06:52:26 -0400

    template: safe math and errors

--- src/impls.rs
+++ src/impls.rs
@@ -4,6 +4,7 @@ use frame::prelude::*;
 impl<T: Config> Pallet<T> {
 	pub fn mint(owner: T::AccountId) -> DispatchResult {
 		let current_count: u32 = CountForKitties::<T>::get().unwrap_or(0);
+		/* 🚧 TODO 🚧: Update this logic to use safe math. */
 		let new_count = current_count + 1;
 		CountForKitties::<T>::set(Some(new_count));
 		Self::deposit_event(Event::<T>::Created { owner });
