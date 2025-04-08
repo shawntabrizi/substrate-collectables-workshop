fc9f7af481c88c14cc8cedf97293584d4b95f780
Shawn Tabrizi <shawntabrizi@gmail.com>
2025-04-08 06:52:24 -0400

    action: learn about pallet struct

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,11 @@ mod tests;
 use frame::prelude::*;
 pub use pallet::*;
 
-/* 🚧 TODO 🚧: Learn about macros used in the `polkadot-sdk`, making pallet development easier. */
 #[frame::pallet(dev_mode)]
 pub mod pallet {
 	use super::*;
 
+	/* 🚧 TODO 🚧: Learn about the Pallet struct. */
 	#[pallet::pallet]
 	pub struct Pallet<T>(core::marker::PhantomData<T>);
 
