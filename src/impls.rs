063ce2807d36901d7cee0f036021ed702a7f3299
Shawn Tabrizi <shawntabrizi@gmail.com>
2025-09-11 11:54:54 +0800

    solution: create kitties owned map

--- src/impls.rs
+++ src/impls.rs
@@ -26,10 +26,10 @@ impl<T: Config> Pallet<T> {
 		let current_count: u32 = CountForKitties::<T>::get();
 		let new_count = current_count.checked_add(1).ok_or(Error::<T>::TooManyKitties)?;
 
-		/* 🚧 TODO 🚧: `append` the `dna` to the `KittiesOwned` storage for the `owner`. */
-
+		KittiesOwned::<T>::append(&owner, dna);
 		Kitties::<T>::insert(dna, kitty);
 		CountForKitties::<T>::set(new_count);
+
 		Self::deposit_event(Event::<T>::Created { owner });
 		Ok(())
 	}
