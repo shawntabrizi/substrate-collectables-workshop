fbec3c4555ace26b3c2360709432811156498000
Shawn Tabrizi <shawntabrizi@gmail.com>
2025-09-11 11:54:52 +0800

    solution: safe math and errors

--- src/impls.rs
+++ src/impls.rs
@@ -4,8 +4,7 @@ use frame::prelude::*;
 impl<T: Config> Pallet<T> {
 	pub fn mint(owner: T::AccountId) -> DispatchResult {
 		let current_count: u32 = CountForKitties::<T>::get().unwrap_or(0);
-		/* 🚧 TODO 🚧: Update this logic to use safe math. */
-		let new_count = current_count + 1;
+		let new_count = current_count.checked_add(1).ok_or(Error::<T>::TooManyKitties)?;
 		CountForKitties::<T>::set(Some(new_count));
 		Self::deposit_event(Event::<T>::Created { owner });
 		Ok(())
