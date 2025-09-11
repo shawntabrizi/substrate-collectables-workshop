301cdb8dd74d8a534e507ac4e4a2298f3f54e5e6
Shawn Tabrizi <shawntabrizi@gmail.com>
2025-09-11 11:54:56 +0800

    template: add do transfer logic

--- src/lib.rs
+++ src/lib.rs
@@ -52,6 +52,11 @@ pub mod pallet {
 		TooManyKitties,
 		DuplicateKitty,
 		TooManyOwned,
+		/* 🚧 TODO 🚧: Add new `Error` variants needed for `do_transfer`:
+			- `TransferToSelf`: for when the `from` and `to` of the transfer is the same.
+			- `NoKitty`: for when a transfer involves a kitty that does not exist.
+			- `NotOwner`: for when a transfer is initiated by someone who is not the current owner.
+		*/
 	}
 
 	#[pallet::call]
